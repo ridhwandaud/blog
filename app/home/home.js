cd9033d5d700cf677851f03d4dd3bea7d7536bc5
ridhwandaud <muhammadridhwandaud@gmail.com>
2016-04-19 20:30:33 +0800

    loading animation

--- app/home/home.js
+++ app/home/home.js
@@ -15,12 +15,18 @@ angular.module('myApp.home', ['ngRoute','firebase'])
     
     var firebaseObj = new Firebase("https://intense-fire-5714.firebaseio.com"); 
     var loginObj = $firebaseAuth(firebaseObj);
+
+    var login = {};
+ 
+    $scope.login = login;
+
     $scope.SignIn = function(user) 
     {
         event.preventDefault();  // To prevent form refresh
         var username =  user.email;
         var password =  user.password;
-        
+        login.loading = true;
+        console.log("email" + user.email)
         loginObj.$authWithPassword ({
                 email: username,
                 password: password
@@ -29,10 +35,12 @@ angular.module('myApp.home', ['ngRoute','firebase'])
                 // Success callback
                 console.log('Authentication successful');
                 CommonProp.setUser(user.password.email);
+                login.loading = false;
                 $location.path('/welcome');
             }, function(error) {
                 // Failure callback
                 console.log('Authentication failure');
+                login.loading = false;
             });
         
      
@@ -51,4 +59,24 @@ angular.module('myApp.home', ['ngRoute','firebase'])
             user = value;
         }
     };
-});
+})
+
+.directive('laddaLoading', [
+    function() {
+        return {
+            link: function(scope, element, attrs) {
+                var Ladda = window.Ladda;
+                var ladda = Ladda.create(element[0]);
+                // Watching login.loading for change
+                scope.$watch(attrs.laddaLoading, function(newVal, oldVal) {
+                    // Based on the value start and stop the indicator
+                    if (newVal) {
+                        ladda.start();
+                    } else {
+                        ladda.stop();
+                    }
+                });
+            }
+        };
+    }
+]);
